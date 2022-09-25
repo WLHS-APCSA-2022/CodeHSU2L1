2e731788f83736dbabbd73a9b3c1040b4b85b385
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-24 20:07:55 -0700

    added grilled cheese class

---
+++ GrilledCheese.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * The GrilledCheese class describes a grilled cheese sandwich.
+ * This file states what every GrilledCheese object will have, but
+ * this file is NOT an object.
+ * 
+ * Every grilled cheese needs a type of bread, a type of cheese,
+ * what temperature to cook the grilled cheese (should it be burned!?),
+ * and if the sandwich should have twice as much cheese as normal.
+ */ 
+
+public class GrilledCheese
+{
+    
+    // Attributes of all Grilled Cheese objects
+    
+    
+    // The methods of the class will go here
+ 
+    // Since this class does not have a main method,
+    // nothing will happen when you run it.
+   
+}
