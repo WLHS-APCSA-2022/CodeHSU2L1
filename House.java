b0d594a31a81c7d3ea48220237b044cb628178da
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-24 20:39:49 -0700

    Create House.java

---
+++ House.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class House here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class House
+{
+    // instance variables - replace the example below with your own
+    
+
+    
+}
